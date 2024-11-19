b924b691d6f2d7231fc73a408e0f1eb9e89623b9
Subhash Kumar Sharma <subhash.bvb@gmail.com>
2024-11-18 19:52:37 -0500

    initial commit

--- src/main/java/com/subhash/externalservice/Client.java
+++ src/main/java/com/subhash/externalservice/Client.java
@@ -1,4 +1,20 @@
 package com.subhash.externalservice;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+
 public class Client {
+
+    public static final String PRODUCT_URL = "http://localhost:7070/sec01/product/%d";
+
+    public static String getProduct(int productId) throws MalformedURLException {
+
+        try(InputStream stream = URI.create(PRODUCT_URL.formatted(productId)).toURL().openStream()) {
+            return new String(stream.readAllBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/com/subhash/externalservice/ExternalServiceDemo.java
+++ src/main/java/com/subhash/externalservice/ExternalServiceDemo.java
@@ -1,4 +1,28 @@
 package com.subhash.externalservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ExternalServiceDemo {
+
+    public static  final Logger LOGGER = LoggerFactory.getLogger(ExternalServiceDemo.class);
+
+    public static void main(String[] args) {
+
+        try(ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
+
+           Future<?> future =  executorService.submit(()->Client.getProduct(1));
+
+           LOGGER.info("Product info :  {}", future.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
